1562ca9428fb2d5baff50baa9083b143d8188747
Andre Sayre <Andre.Sayre@microsoft.com>
2015-01-27 18:41:03 -0800

    Initial commit

---
+++ VSPackage5/Guids.cs
@@ -0,0 +1,16 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.VSPackage5
+{
+    static class GuidList
+    {
+        public const string guidVSPackage5PkgString = "e48636e4-efbc-4ec0-8644-9cc83d560100";
+        public const string guidVSPackage5CmdSetString = "417ba3fd-716c-4ce9-9e35-9cb88d73706d";
+        public const string guidVSPackage5EditorFactoryString = "3465b16c-6743-45de-aef5-6b9e184d0e4e";
+
+        public static readonly Guid guidVSPackage5CmdSet = new Guid(guidVSPackage5CmdSetString);
+        public static readonly Guid guidVSPackage5EditorFactory = new Guid(guidVSPackage5EditorFactoryString);
+    };
+}
---
+++ VSPackage5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestTestPkg")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("TestTestPkg")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("VSPackage5_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a77a81bcac2e9bffa90fb792d720549a1f5b8cfaf7bd2881913a0927f12d1f0d2ac0bc434ffc2ccaa7e209d94ff397e96ed14d20d966047ae14fc31777d71a6eb388a7b1df6207cfe85986369662bc745f239204c781cc22e66f5ba479d03fa7bbb5f66203b33117f9f21d34752ebcd5b76c16c0db6f256bef19bca4f596aadf")]
+[assembly: InternalsVisibleTo("VSPackage5_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a77a81bcac2e9bffa90fb792d720549a1f5b8cfaf7bd2881913a0927f12d1f0d2ac0bc434ffc2ccaa7e209d94ff397e96ed14d20d966047ae14fc31777d71a6eb388a7b1df6207cfe85986369662bc745f239204c781cc22e66f5ba479d03fa7bbb5f66203b33117f9f21d34752ebcd5b76c16c0db6f256bef19bca4f596aadf")]
